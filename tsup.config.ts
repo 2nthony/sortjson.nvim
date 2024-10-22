8dd492f7703351140c62db698125e8332f45bf66
2nthony <ant.hu@outlook.com>
2024-10-22 20:24:56 +0800

    clean js files

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['src/cli.ts'],
-  outDir: 'lua/sortjson',
-  format: ['cjs'],
-  target: 'node16',
-  minify: true,
-})
